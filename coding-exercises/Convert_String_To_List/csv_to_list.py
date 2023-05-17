6001d1a6f35dcc6c2cf72ca4058c58a268a8ac47
Jason_K <jasonmkahraman@gmail.com>
2023-05-17 09:33:29 -0400

    added new things

--- coding-exercises/Convert_String_To_List/csv_to_list.py
+++
@@ -1,7 +0,0 @@
-#given string
-string1="abc,def,ghi"
-print("Actual CSV String: ",string1)
-print("Type of string: ",type(string1))
- 
-#spliting string1 into list with ',' as the parameter
-print("CSV coverted to list :",string1.split(','))
